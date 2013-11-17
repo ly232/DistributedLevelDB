4d327bd6c745aef30b49519dd8a35ed9db350938
Lin Yang <ly232@cornell.edu>
2013-11-17 15:02:56 -0800

    .

---
+++ include/commit.h
@@ -0,0 +1,16 @@
+//commit.h
+//2 phase commit header file
+#ifndef _commit_h
+#define _commit_h
+#ifndef _common_h
+#include "common.h"
+#endif
+class TwoPhaseCommit
+{
+public:
+  TwoPhaseCommit();
+  ~TwoPhaseCommit();
+private:
+  
+};
+#endif
---
+++ server/commit/commit.cpp
@@ -0,0 +1,5 @@
+//commit.cpp
+//2 phase commit implementation file
+#include "commit.h"
+
+
